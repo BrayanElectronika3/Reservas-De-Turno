c230ec49b4ccdc4aa9a9ef4cc5d24c499860bd89
Brayan Electronika 3 <brayanelectronika03@gmail.com>
2024-11-08 16:26:12 -0500

    ADD component NotFoundPage

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage/HomePage'
 import LoginPage from './pages/LoginPage/LoginPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import ReservationPage from './pages/ReservationPage/ReservationPage'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 import PrivateGuard from './guards/PrivateGuard'
 
 const AppRouter = () => {
@@ -13,14 +14,11 @@ const AppRouter = () => {
                 <Route path='/:IDTenant' element={<HomePage/>}/>
                 <Route path='/login' element={<LoginPage/>}/>
                 <Route path='/register' element={<RegisterPage/>}/>
-
                 <Route element={<PrivateGuard/>}>
                     <Route path='/reservation' element={<ReservationPage/>}/>
                 </Route>
-
-                <Route path='/404' element={<h1>Page not found</h1>}/>
+                <Route path='/404' element={<NotFoundPage/>}/>
                 <Route path="*" element={<Navigate to={'/404'}/>} />
-
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+const NotFoundPage = () => {
+    return (
+        <h1>Page not found</h1>
+    )
+}
+
+export default NotFoundPage
