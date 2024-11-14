cde12c73879cb8253437bee16d12d591cf98d436
Brayan Electronika 3 <brayanelectronika03@gmail.com>
2024-11-14 15:25:37 -0500

    Refactorizacion de animaciones y eliminacion de Scroll

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
+import PropTypes from 'prop-types'
 
 import HomePage from './pages/HomePage/HomePage'
 import LoginPage from './pages/LoginPage/LoginPage'
@@ -11,21 +12,46 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
 import PrivateGuard from './guards/PrivateGuard'
 
+// Animacion
+const Animated = ({ children }) => {
+    const pageVariants = { initial: { opacity: 0, x: 500 }, enter: { opacity: 1, x: 0 }, exit: { opacity: 0, x: -500 }, }
+    const pageTransition = { type: 'tween', ease: 'anticipate', duration: 1.5, }
+    const handleAnimationStart = () => { document.body.style.overflow = 'hidden' }
+    const handleAnimationComplete = () => { document.body.style.overflow = '' }
+
+    return (
+        <motion.div 
+            initial="initial" 
+            animate="enter" 
+            exit="exit" 
+            variants={pageVariants} 
+            transition={pageTransition} 
+            onAnimationStart={handleAnimationStart} 
+            onAnimationComplete={handleAnimationComplete}
+        > 
+            {children} 
+        </motion.div>
+    )
+}
+
+Animated.propTypes = {
+    children: PropTypes.node
+}
+
+// Asignacion de animaciones
 const AnimatedRoutes = () => {
     const location = useLocation()
-    const pageVariants = { initial: { opacity: 0, x: 400 }, enter: { opacity: 1, x: 0 }, exit: { opacity: 0, x: -400 }, }
-    const pageTransition = { type: 'tween', ease: 'anticipate', duration: 1.5, }
 
     return (
         <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
                 <Route path='/:tenant' element={<HomePage/>}/>
-                <Route path='/login' element={ <motion.div initial="initial" animate="enter" exit="exit" variants={pageVariants} transition={pageTransition} > <LoginPage /> </motion.div> } />
-                <Route path='/consultReservation' element={ <motion.div initial="initial" animate="enter" exit="exit" variants={pageVariants} transition={pageTransition}> <ConsultReservationPage /> </motion.div> } />
-                <Route path='/register' element={ <motion.div initial="initial" animate="enter" exit="exit" variants={pageVariants} transition={pageTransition}> <RegisterPage /> </motion.div> } />
+                <Route path='/login' element={ <Animated> <LoginPage/> </Animated> } />
+                <Route path='/consultReservation' element={ <Animated> <ConsultReservationPage/> </Animated> } />
+                <Route path='/register' element={ <Animated> <RegisterPage/> </Animated> } />
                 <Route element={<PrivateGuard/>}>
-                    <Route path='/reservation' element={ <motion.div initial="initial" animate="enter" exit="exit" variants={pageVariants} transition={pageTransition}> <ReservationPage /> </motion.div> } />
-                    <Route path='/summary' element={ <motion.div initial="initial" animate="enter" exit="exit" variants={pageVariants} transition={pageTransition}> <SummaryPage /> </motion.div> }/>
+                    <Route path='/reservation' element={ <Animated> <ReservationPage/> </Animated> } />
+                    <Route path='/summary' element={ <Animated> <SummaryPage/> </Animated> }/>
                 </Route>
                 <Route path='/404' element={<NotFoundPage/>}/>
                 <Route path="*" element={<Navigate to={'/404'}/>} />
@@ -34,6 +60,7 @@ const AnimatedRoutes = () => {
     )
 }
 
+// App router
 const AppRouter = () => {
     return (
         <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
